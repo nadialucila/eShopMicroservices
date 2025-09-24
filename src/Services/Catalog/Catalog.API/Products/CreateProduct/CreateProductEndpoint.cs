8088e7267b21c15994ed94decd9728d5b43b41fa
nadialucila <nadia.bergara17@gmail.com>
2025-09-24 15:52:28 -0300

    add: carter and mediatr program.cs config, endpoint logic

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,4 @@
-﻿using Carter;
-
-namespace Catalog.API.Products.CreateProduct;
+﻿namespace Catalog.API.Products.CreateProduct;
 
 public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
@@ -10,6 +8,23 @@ public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        throw new NotImplementedException();
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+        {
+            //mapeamos la request al command para que lo pueda procesar su handler x mediatr
+            var command = request.Adapt<CreateProductCommand>();
+
+            //enviamos x mediatr y esperamos resultado del handle
+            var result = await sender.Send(command);
+
+            //volvemos a mapear el resultado al response
+            var response = result.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{response.Id}", response);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
     }
 }
