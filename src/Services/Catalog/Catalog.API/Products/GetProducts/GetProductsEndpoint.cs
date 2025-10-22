748d468a6e4eac08d3ee409700ba642c3b4ca86c
nadialucila <nadia.bergara17@gmail.com>
2025-10-22 15:28:43 -0300

    add: get products endpoint

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -1,6 +1,24 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsRequest(); //not necessary. only for best practices.
+public record GetProductsResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndpoint : ICarterModule
 {
-    public class GetProductsEndpoint
+    public void AddRoutes(IEndpointRouteBuilder app)
     {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductsQuery());
+
+            var response = result.Adapt<GetProductsResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProducts")
+        .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get Products")
+        .WithDescription("Get Products");
     }
 }
