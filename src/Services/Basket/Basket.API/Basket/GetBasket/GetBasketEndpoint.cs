6b59387e2eca06b458fb5137906e48fb588d31f9
nadialucila <nadia.bergara17@gmail.com>
2025-11-06 17:13:40 -0300

    add: get basket endpoint impl

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -8,9 +8,18 @@ public class GetBasketEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/basket", async (ISender sender) =>
+        app.MapGet("/basket/{username}", async (string username, ISender sender) =>
         {
+            var result = await sender.Send(new GetBasketQuery(username));
 
-        });
+            var response = result.Adapt<GetBasketResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetBasket")
+        .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get basket")
+        .WithDescription("Get basket");
     }
 }
