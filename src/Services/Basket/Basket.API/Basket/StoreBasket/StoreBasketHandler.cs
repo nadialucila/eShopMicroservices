5d4d7ae4e568cbe8a2dc18a29153e4474aa5beed
nadialucila <nadia.bergara17@gmail.com>
2025-11-07 17:07:54 -0300

    add: store basket handler base logic

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,5 +1,27 @@
 ﻿namespace Basket.API.Basket.StoreBasket;
 
-public class StoreBasketHandler
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+
+public record StoreBasketResult(string Username);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 {
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart should not be null.");
+        RuleFor(x => x.Cart.Username).NotEmpty().WithMessage("Username is required.");
+    }
+}
+
+public class StoreBasketHandler(IDocumentSession session) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart Cart = command.Cart;
+
+        //save basket to db - upsert
+        //update cache
+
+        return new StoreBasketResult("user");
+    }
 }
