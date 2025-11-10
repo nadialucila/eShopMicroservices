21952bcc2192911b90bc2f527b9cceb4f1f93c75
nadialucila <nadia.bergara17@gmail.com>
2025-11-10 16:20:03 -0300

    refactor: remove injection - rearrange program class

--- src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -4,7 +4,7 @@ public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
 
 public record GetBasketResult(ShoppingCart Cart);
 
-public class GetBasketQueryHandler(IDocumentSession session) : IQueryHandler<GetBasketQuery, GetBasketResult>
+public class GetBasketQueryHandler() : IQueryHandler<GetBasketQuery, GetBasketResult>
 {
     public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
     {
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -3,25 +3,40 @@ var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 var database = builder.Configuration.GetConnectionString("Database")!;
 
-//services
-builder.Services.AddCarter();
-builder.Services.AddMarten(opts =>
-{
-    opts.Connection(database);
-}).UseLightweightSessions();
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
-builder.Services.AddValidatorsFromAssembly(assembly);
-builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+ConfigureServices(builder);
 
 var app = builder.Build();
 
-//http pipeline
-app.MapCarter();
-app.UseExceptionHandler(options => { });
+ConfigurePipeline(app);
 
 app.Run();
+
+#region Services
+void ConfigureServices(WebApplicationBuilder builder){
+    builder.Services.AddCarter();
+
+    builder.Services.AddMarten(opts =>
+    {
+        opts.Connection(database);
+    }).UseLightweightSessions();
+
+    builder.Services.AddMediatR(config =>
+    {
+        config.RegisterServicesFromAssembly(assembly);
+        config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+    });
+
+    builder.Services.AddValidatorsFromAssembly(assembly);
+
+    builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+}
+#endregion
+
+#region Http Pipeline
+void ConfigurePipeline(WebApplication app)
+{
+    app.MapCarter();
+    app.UseExceptionHandler(options => { });
+}
+#endregion
